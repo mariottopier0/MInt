35c21194fe575ecfd87b9600ecd08deb6d385f76
mariottopier0 <133961826+mariottopier0@users.noreply.github.com>
2023-11-17 11:48:18 +0100

    Add files via upload

--- R/MInt.R
+++ R/MInt.R
@@ -34,9 +34,6 @@ mint <- function(y, x, fmla= ~1) {
   return(mfit);
 }
 
-
-
-
 ## Function implementations
 
 #' @title Estimate parameters
@@ -76,7 +73,6 @@ mint <- function(y, x, fmla= ~1) {
 #' @import MASS
 #' @export
 estimate <- function(mfit) {
-  
   
   # Read data, initialize optimizer, and initialize parameters if 
   # not already done.
@@ -121,8 +117,8 @@ bootstrap <- function(mfit,nboot=10,seed=1) {
     mfit_copy <- mfit;
     
     # Resample
-    bsidx <- sample.int(nrow(mfit$data$y), nrow(mfit$data$y), TRUE)
-    mfit_copy$data$y <- mfit$data$y[bsidx,];
+    bsidx <- sample.int(nrow(mfit$data$response), nrow(mfit$data$response), TRUE)
+    mfit_copy$data$y <- mfit$data$response[bsidx,];
     mfit_copy$data$x <- as.matrix(mfit$data$x[bsidx,]);
     mfit_copy$data$xd <- as.data.frame(mfit$data$xd[bsidx,], row.names=rownames(mfit$data$xd), col.names=colnames(mfit$data$xd));
     
@@ -162,10 +158,10 @@ initialize_optimizer <- function(mfit) {
     stop("Uninitialized response matrix.")
   }
   
-  o <- ncol(mfit$data$y);
-  n <- nrow(mfit$data$y);
+  o <- ncol(mfit$data$response);
+  n <- nrow(mfit$data$response);
   
-  mfit$optim$objective_constant <- -sum(lfactorial(mfit$data$y)) - o*n*log(2*pi)/2 + (o^2)*log(n/4);
+  mfit$optim$objective_constant <- -sum(lfactorial(mfit$data$response)) - o*n*log(2*pi)/2 + (o^2)*log(n/4);
   mfit$optim$tolerance <- 1e-4;
   mfit$optim$lambda <- 0.01;
   mfit$optim$itr <- 0;
@@ -186,13 +182,13 @@ initialize_parameters <- function(mfit) {
   # Initialize the parameters.
   # beta
   # Start with a log-normal fit. 
-  mfit$param$xb <- matrix(, nrow=nrow(mfit$data$y), ncol=ncol(mfit$data$y));
+  mfit$param$xb <- matrix(, nrow=nrow(mfit$data$response), ncol=ncol(mfit$data$response));
   mfit <- initialize_beta(mfit);
   mfit <- xb(mfit);
   
   # w
   # Set to be the residuals of the log-normal fit.
-  mfit$param$w <- log(mfit$data$y+1) - mfit$param$xb;
+  mfit$param$w <- log(mfit$data$response+1) - mfit$param$xb;
   
   # mu
   mfit <- update_mu(mfit);
@@ -201,14 +197,14 @@ initialize_parameters <- function(mfit) {
   # Set to be cov(w) and pseudoinverse of cov(w)
   mfit$param$S <- pop_cov(mfit$param$w, mfit$param$mu);
   mfit$param$P <- MASS::ginv(mfit$param$S); 
-  rownames(mfit$param$P) <- colnames(mfit$data$y);
-  colnames(mfit$param$P) <- colnames(mfit$data$y);
-  rownames(mfit$param$S) <- colnames(mfit$data$y);
-  colnames(mfit$param$S) <- colnames(mfit$data$y);
+  rownames(mfit$param$P) <- colnames(mfit$data$response);
+  colnames(mfit$param$P) <- colnames(mfit$data$response);
+  rownames(mfit$param$S) <- colnames(mfit$data$response);
+  colnames(mfit$param$S) <- colnames(mfit$data$response);
   mfit$optim$oldP <- mfit$param$P; # Store a copy of P for convergence checking.
   
   # Update hyperparameter lambda to its conditional mode.
-  mfit$optim$lambda <- 2*(length(mfit$param$mu)^2)/( nrow(mfit$data$y)*sum(abs(mfit$param$P)) );
+  mfit$optim$lambda <- 2*(length(mfit$param$mu)^2)/( nrow(mfit$data$response)*sum(abs(mfit$param$P)) );
   
   mfit$param$ready <- TRUE;
   return(mfit)
@@ -221,8 +217,8 @@ initialize_parameters <- function(mfit) {
 update_beta <- function(mfit) {
   
   if (!mfit$optim$clamp$beta) {
-    for (j in 1 : ncol(mfit$data$y)) {
-      y <- mc(mfit$data$y,j); # extract a response vector
+    for (j in 1 : ncol(mfit$data$response)) {
+      y <- mc(mfit$data$response,j); # extract a response vector
       w <- mc(mfit$param$w,j); # extract the corresponding latent abundance vector
       fmla <- update(mfit$data$fmla, y ~ . + offset(w));
       
@@ -241,8 +237,8 @@ update_beta <- function(mfit) {
 
 update_mu <- function(mfit) {
   if (!mfit$optim$clamp$mu){
-    mfit$param$mu <- matrix(replicate(ncol(mfit$data$y),0),nrow=1); #matrix(colMeans(mfit$param$w),nrow=1);
-    colnames(mfit$param$mu) <- colnames(mfit$data$y);
+    mfit$param$mu <- matrix(replicate(ncol(mfit$data$response),0),nrow=1); #matrix(colMeans(mfit$param$w),nrow=1);
+    colnames(mfit$param$mu) <- colnames(mfit$data$response);
   }
   
   return(mfit)
@@ -255,8 +251,8 @@ update_P <- function(mfit) {
     mfit$param$P <- res$wi;
     mfit$param$S <- res$w;
     
-    rownames(mfit$param$P) <- colnames(mfit$data$y);
-    colnames(mfit$param$P) <- colnames(mfit$data$y);
+    rownames(mfit$param$P) <- colnames(mfit$data$response);
+    colnames(mfit$param$P) <- colnames(mfit$data$response);
   }
   
   return(mfit);
@@ -266,7 +262,7 @@ update_w <- function(mfit) {
   if (!mfit$optim$clamp$w){
     for (ridx in 1:nrow(mfit$param$w)){
       wopt <- trust::trust(objfun=wfun, parinit=mfit$param$w[ridx,], 
-                    rinit=5, rmax=100, minimize=FALSE, ridx=ridx, y=mfit$data$y, param=mfit$param);
+                    rinit=5, rmax=100, minimize=FALSE, ridx=ridx, y=mfit$data$response, param=mfit$param);
       mfit$param$w[ridx,] <- wopt$argument; # the maximizer.
     }
   }
@@ -292,12 +288,12 @@ check_convergence <- function(mfit) {
 initialize_beta <- function(mfit) {
   # Compute this as a loop in case there are response-specific 
   # design matrices (i.e. mfit$data$x[[i]] != mfit$data$x[[j]], for i != j)
-  mfit$param$beta <- matrix(, nrow=ncol(mfit$data$x[[1]]), ncol=ncol(mfit$data$y));
-  for (j in 1 : ncol(mfit$data$y)) {
-    mfit$param$beta[,j] <- MASS::ginv(mfit$data$x[[j]]) %*% log(mfit$data$y[,j] + 1);
+  mfit$param$beta <- matrix(, nrow=ncol(mfit$data$x[[1]]), ncol=ncol(mfit$data$response));
+  for (j in 1 : ncol(mfit$data$response)) {
+    mfit$param$beta[,j] <- MASS::ginv(mfit$data$x[[j]]) %*% log(mfit$data$response[,j] + 1);
   }
   rownames(mfit$param$beta) <- colnames(mfit$data$x[[1]]);
-  colnames(mfit$param$beta) <- colnames(mfit$data$y);
+  colnames(mfit$param$beta) <- colnames(mfit$data$response);
   return(mfit);
 }
 
@@ -305,15 +301,15 @@ xb <- function(mfit) {
   # Assumes mfit$param$xb is already preallocated.
   # Compute this as a loop in case there are response-specific 
   # design matrices (i.e. mfit$data$x[[i]] != mfit$data$x[[j]], for i != j)
-  for (j in 1 : ncol(mfit$data$y)) {
+  for (j in 1 : ncol(mfit$data$response)) {
     mfit$param$xb[,j] <- mfit$data$x[[j]] %*% mc(mfit$param$beta,j);
   }
-  dimnames(mfit$param$xb) <- dimnames(mfit$data$y);
+  dimnames(mfit$param$xb) <- dimnames(mfit$data$response);
   return(mfit);
 }
 
 read_response <- function(mfit, row.names="Observations") {
-  mfit$data$y <- as.matrix(read.table(mfit$data$response, header=TRUE, row.names=row.names));
+  mfit$data$response <- as.matrix(read.table(mfit$data$response, header=TRUE, row.names=row.names));
   return(mfit);
 }
 
@@ -323,11 +319,11 @@ read_design <- function(mfit, row.names="Observations") {
   # specific design matrices.
   #   - Design matrix files for each response should be labeled as
   #     <response_name>.txt, where <response_name> is an element of 
-  #     colnames(mfit$data$y)
+  #     colnames(mfit$data$response)
   # If it's a file, then this it should be a single design matrix that 
   # all response variables share.
   
-  if (is.null(mfit$data$y)) {
+  if (is.null(mfit$data$response)) {
     stop("Uninitialized response matrix. Unknown response variable names.");
   }
   
@@ -337,11 +333,11 @@ read_design <- function(mfit, row.names="Observations") {
   if (file.info(mfit$data$design)$isdir) {
     
     # Change directory to the design directory, load all of the design matrices in the order of
-    # colnames(mfit$data$y), and change back to the original directory.
+    # colnames(mfit$data$response), and change back to the original directory.
     old <- getwd();
     setwd(mfit$data$design)
     
-    for (rn in colnames(mfit$data$y)){
+    for (rn in colnames(mfit$data$response)){
       mfit$data$xd[[rn]] <- model.frame(mfit$data$fmla, read.table(paste0(rn, ".txt"), header=TRUE, row.names=row.names));
       mfit$data$x[[rn]] <- model.matrix(mfit$data$fmla, mfit$data$xd[[rn]]);
     }
@@ -350,13 +346,13 @@ read_design <- function(mfit, row.names="Observations") {
     
   } else if (file.exists(mfit$data$design)) {
     
-    # Load the design matrix and copy it for all responses in colnames(mfit$data$y)
+    # Load the design matrix and copy it for all responses in colnames(mfit$data$response)
     # We are copying it so that all functions deal with the same data structure regardless
     # of whether there is a single design matrix or one for each response.
     xd <- model.frame(mfit$data$fmla, read.table(mfit$data$design, header=TRUE, row.names=row.names));
     x <- model.matrix(mfit$data$fmla, xd);
     
-    for (rn in colnames(mfit$data$y)){
+    for (rn in colnames(mfit$data$response)){
       mfit$data$xd[[rn]] <- xd;
       mfit$data$x[[rn]] <- x;
     }
@@ -407,12 +403,12 @@ wfun <- function(wrow, ...){
 }
 
 evaluate_objective <- function(mfit) {
-  n <- nrow(mfit$data$y);
+  n <- nrow(mfit$data$response);
   eta <- exp(mfit$param$xb + mfit$param$w);
   dv <- determinant(mfit$param$P,logarithm=TRUE);
   ldP <- dv$modulus;
   
-  poisson_component <- sum(mfit$data$y*log(eta) - eta);
+  poisson_component <- sum(mfit$data$response*log(eta) - eta);
   gaussian_component <- n*ldP/2 - n*sum(pop_cov(mfit$param$w,mfit$param$mu)*mfit$param$P)/2;
   laplacian_component <- -n*mfit$optim$lambda*sum(abs(mfit$param$P))/2;
   
